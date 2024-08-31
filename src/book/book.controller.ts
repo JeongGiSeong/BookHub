516cf6cb9f94b373f8ec58720c753544f7ce263f
JeongGiSeong <rltjd8714@gmail.com>
2024-09-01 04:09:09 +0900

    chore: Add book module, controller, service, and schemas

---
+++ src/book/book.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { BookService } from './book.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { Book } from './schemas/book.schema';
+
+import { Query as ExpressQuery } from 'express-serve-static-core';
+
+@Controller('books')
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Get(':id')
+  async getBookById(@Param('id') id: string) {
+    return this.bookService.findById(id);
+  }
+
+  @Get()
+  async getAllBooks(@Query() query: ExpressQuery) {
+    return this.bookService.findAll(query);
+  }
+
+  @Post()
+  async createBook(@Body() book: CreateBookDto) {
+    return this.bookService.create(book);
+  }
+
+  @Patch(':id')
+  async updateBook(@Param('id') id: string, @Body() book: UpdateBookDto): Promise<Book> {
+    return this.bookService.updateById(id, book);
+  }
+
+  @Delete(':id')
+  async deleteBook(@Param('id') id: string): Promise<Book> {
+    return this.bookService.deleteById(id);
+  }
+}
---
+++ src/book/book.service.ts
@@ -0,0 +1,64 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { Book } from './schemas/book.schema';
+import mongoose, { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Query } from 'express-serve-static-core';
+
+@Injectable()
+export class BookService {
+  constructor(
+    @InjectModel(Book.name)
+    private bookModel: Model<Book>
+  ) {}
+
+  async findAll(query: Query): Promise<Book[]> {
+    const resPerPage = 2;
+    const curPage = Number(query.page) || 1;
+    const skip = (curPage - 1) * resPerPage;
+
+    const keyword = query.keyword
+      ? {
+          title: {
+            $regex: query.keyword,
+            $options: 'i',
+          },
+        }
+      : {};
+
+    return await this.bookModel
+      .find({ ...keyword })
+      .limit(resPerPage)
+      .skip(skip);
+  }
+
+  async create(book: Book): Promise<Book> {
+    const newBook = await this.bookModel.create(book);
+    return newBook.save();
+  }
+
+  async findById(id: string): Promise<Book> {
+    const isValidId = mongoose.isValidObjectId(id);
+
+    if (!isValidId) {
+      throw new BadRequestException('Invalid book ID');
+    }
+
+    const book = await this.bookModel.findById(id);
+    if (!book) {
+      throw new NotFoundException('Book not found');
+    }
+    return book;
+  }
+
+  async updateById(id: string, book: Book): Promise<Book> {
+    return await this.bookModel.findByIdAndUpdate(id, book, {
+      new: true,
+      runValidators: true,
+    });
+  }
+
+  async deleteById(id: string): Promise<Book> {
+    return await this.bookModel.findByIdAndDelete(id);
+  }
+}
